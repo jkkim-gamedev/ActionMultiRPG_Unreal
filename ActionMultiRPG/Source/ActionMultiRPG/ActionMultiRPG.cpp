2e7023bfd66a61facbbaf8dd0e166540d452268b
JongKyeong Kim <jk.kim.games@gmail.com>
2025-07-31 16:56:17 +0900

    initial setting

---
+++ ActionMultiRPG/Source/ActionMultiRPG/ActionMultiRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionMultiRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionMultiRPG, "ActionMultiRPG" );
